06bd9bb641d990670d18facf6bf75aa37fca0a5a
VictorPochettini <vpochettini@gmail.com>
2024-11-13 23:04:06 -0300

    Update Calculadora.cs
    Um pequeno avanço até eu ficar cansado de tanto programar hoje (realmente esse código não é quase nada, mas já tinha feito muitos outros programas antes)

--- Calculadora.cs
+++ Calculadora.cs
@@ -4,6 +4,9 @@ class Calculadora
 {
     static void Main()
     {
+        string conta;
+
         Console.WriteLine("Digite a operação desejada no modelo a+b, como 4+2 ou 5-3 ou 7/4\n");
+        conta = Console.ReadLine();
     }
 }
